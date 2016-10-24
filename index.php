0ad90a95ef6ada3f68319c2a38ded19af52b1b78
Andriy <popovich668@gmail.com>
2016-10-24 12:42:10 +0300

    иролрплио

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MediaCompetition</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="images/unnamed.png" type="image/x-icon">
+    <script type="text/javascript" src="js/common.js"></script>
+</head>
+<body>
+<div class="wrapper">
+    <?php
+    include ("header.php");
+    ?>
+    <div class="content">
+        <div class="resize">
+            <div class="first">
+                <div class="name_block">
+                    <h2>Конкурс фото</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                </div>
+            </div>
+            <div class="second">
+                <div class="name_block">
+                    <h2>Конкурс відео</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                </div>
+            </div>
+            <div class="third">
+                <div class="name_block">
+                    <h2>Конкурс аудіо</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Богдана
+ * Date: 11.10.2016
+ * Time: 18:07
+ */
