f52038390e0ec8aba4409d834c91bd60627be780
uts 'bgdhjg <bzaychuk@ukr.net>
2016-10-26 09:41:20 +0300

    bogdana ver1.0

---
+++ photo.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MediaCompetition</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/content.css">
+    <link rel="shortcut icon" href="images/unnamed.png" type="image/x-icon">
+</head>
+<body>
+<div class="wrapper">
+    <?php
+    include ("header.php");
+    ?>
+    <div class="content">
+        <div class="resize">
+            <div class="first">
+                <div class="name_block">
+                    <h2>Конкурс №1</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                    <a href="more.php" class="more">Більше...</a>
+                </div>
+            </div>
+            <div class="second">
+                <div class="name_block">
+                    <h2>Конкурс №2</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                    <a href="more.php" class="more">Більше...</a>
+                </div>
+            </div>
+            <div class="third">
+                <div class="name_block">
+                    <h2>Конкурс №3</h2>
+                </div>
+                <div class="inform">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <img src="/images/1.jpg">
+                    <img src="/images/2.jpeg">
+                    <a href="more.php" class="more">Більше...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+</body>
+</html>
