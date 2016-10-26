fbc9e10b06e5afd6a3d96942ef45fdfba07a2224
uts 'bgdhjg <bzaychuk@ukr.net>
2016-10-26 09:46:10 +0300

    bogdana ver2.0

---
+++ audio.php
@@ -0,0 +1,141 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MediaCompetition - Audio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="images/unnamed.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/content.css">
+    <!--<script type="text/javascript" src="/js/Pagination/tests/lib/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/Pagination/jquery.simplePagination.js"></script>
+    <link type="text/css" rel="stylesheet" href="/js/Pagination/simplePagination.css"/>-->
+</head>
+<body>
+<div class="wrapper">
+    <?php
+    include ("header.php");
+    ?>
+    <div class="content">
+        <div class="resize">
+            <center><h1 class="site_header">Конкурс аудіо</h1></center>
+            <div class="first">
+                <div class="name_block">
+                    <h2>Конкурс №1</h2>
+                </div>
+                <div class="inform_audio">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/adele-rolling_in_the_deep.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/adele-rolling_in_the_deep.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/Afric_Simone_Hafanana.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/Afric_Simone_Hafanana.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss - Stones .mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss - Stones .mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss-Hurricane.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss-Hurricane.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <a href="more.php" class="more">більше записів</a>
+                </div>
+            </div>
+            <div class="second">
+                <div class="name_block">
+                    <h2>Конкурс №2</h2>
+                </div>
+                <div class="inform_audio">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/adele-rolling_in_the_deep.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/adele-rolling_in_the_deep.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/Afric_Simone_Hafanana.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/Afric_Simone_Hafanana.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss - Stones .mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss - Stones .mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss-Hurricane.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss-Hurricane.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <a href="more.php" class="more">більше записів</a>
+                </div>
+            </div>
+            <div class="third">
+                <div class="name_block">
+                    <h2>Конкурс №3</h2>
+                </div>
+                <div class="inform_audio">
+                    <p class="date">Конкурс діє з 11.10.16 по 31.10.16</p>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/adele-rolling_in_the_deep.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/adele-rolling_in_the_deep.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/Afric_Simone_Hafanana.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/Afric_Simone_Hafanana.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss - Stones .mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss - Stones .mp3">Скачайте музыку</a>.
+                    </audio>
+                    <center><b class="mus_p">Adele-Rolling in the deep</b></center>
+                    <audio controls>
+                        <source src="audio/The Hardkiss-Hurricane.mp3" type="audio/mpeg">
+                        Тег audio не поддерживается вашим браузером.
+                        <a href="audio/The Hardkiss-Hurricane.mp3">Скачайте музыку</a>.
+                    </audio>
+                    <a href="more.php" class="more">більше записів</a>
+                </div>
+                <!--<div id="light-pagination" class="pagination"></div>
+                <script type='text/javascript'>
+                    $(window).load(function(){
+                        $('#light-pagination').pagination({
+                            items: 100,
+                            itemsOnPage: 5,
+                            hrefTextPrefix: '',
+                            hrefTextSuffix: '.html',
+                            prevText: 'Начало',
+                            nextText: 'Конец',
+                            cssStyle: 'light-theme'
+                        });
+
+                    });
+                </script>-->
+            </div>
+        </div>
+    </div>
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+</body>
+</html>
