5c5cbf3603a8a6abdf88ec4aaa23dc58d8187803
uts 'bgdhjg <bzaychuk@ukr.net>
2016-10-26 09:54:31 +0300

    bogdana ver3.0

---
+++ more.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MediaCompetition - Video</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="shortcut icon" href="images/unnamed.png" type="image/x-icon">
+
+</head>
+<body>
+<div class="wrapper">
+    <?php
+    include ("header.php");
+    ?>
+    <div class="content">
+        <div class="resize">
+            <center><h1 class="site_header">Конкурс відео</h1></center>
+            <div class="first">
+                <div class="name_block">
+                    <h2>Конкурс №1</h2>
+                </div>
+            </div>
+            <div class="second">
+                <div class="name_block">
+                    <h2>Конкурс №2</h2>
+                </div>
+            </div>
+            <div class="third">
+                <div class="name_block">
+                    <h2>Конкурс №3</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+</body>
+</html>
