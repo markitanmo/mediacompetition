d300ed6839775517902e14f377dec762bfe32140
markitanmo <markitanmo@yandex.ru>
2016-10-26 09:22:20 +0300

    markitan v2.0

---
+++ header.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+
+<header class="main_header">
+    <div class="log_in">
+        <button>Log in</button>
+    </div>
+    <div class="logo">
+        <img src="\images\media_header.png" class="img-logo">
+        <h1>MediaCompetition</h1>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="video.php">Video</a></li>
+            <li><a href="photo.php">Photo</a></li>
+            <li><a href="audio.php">Music</a></li>
+        </ul>
+    </div>
+</header>
